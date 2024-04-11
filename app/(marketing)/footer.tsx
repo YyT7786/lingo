66e8bb625f1d0f3d6145be71cc1e7dd84e692cd3
YapYanTing <yanting@boostorder.com>
2024-04-11 12:50:55 +0800

    Footer added

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button variant="ghost">
+                    <Image
+                        src="hr.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Croatian
+                </Button>
+                <Button className="border-4" variant="ghost">
+                    <Image
+                        src="es.svg"
+                        alt="Spain"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Spanish
+                </Button>
+                <Button variant="ghost">
+                    <Image
+                        src="fr.svg"
+                        alt="Franch"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    French
+                </Button>
+                <Button variant="ghost">
+                    <Image
+                        src="it.svg"
+                        alt="Italy"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Italy
+                </Button>
+                <Button variant="ghost">
+                    <Image
+                        src="jp.svg"
+                        alt="Japan"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Japan
+                </Button>
+            </div>
         </footer>
     );
 };
