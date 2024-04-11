eb897f73ba11ec000f645f45d0bcaaa68aa73c6a
YapYanTing <yanting@boostorder.com>
2024-04-11 11:39:57 +0800

    Sign in & out button added

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,67 @@
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import {
+    ClerkLoaded,
+    ClerkLoading,
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton,
+    auth
+} from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
+    const { userId } = auth();
+
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+                        Lingo
+                    </h1>
+                </div>
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-forground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    {/* <SignedIn>
+                        <UserButton
+                            afterSignOutUrl="/"
+                        />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton
+                            mode="modal"
+                            afterSignInUrl="/learn"
+                            afterSignUpUrl="/learn"
+                        >
+                            <Button size="lg" variant="ghost">
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut> */}
+
+                    <div>
+                        {!userId && (
+                            <SignInButton
+                                mode="modal"
+                            >
+                                <Button size="lg" variant="ghost">
+                                    Login
+                                </Button>
+                            </SignInButton>
+                        )}
+                        <UserButton
+                            afterSignOutUrl="/"
+                        />
+                    </div>
+
+                </ClerkLoaded>
+            </div>
         </header>
     );
 };
