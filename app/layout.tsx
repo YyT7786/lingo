c05e8a2e9a4f20ac975785033e9643aaf493db3f
YapYanTing <yanting@boostorder.com>
2024-07-05 22:17:37 +0800

    Add card & challenge & question bubble component in quiz page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 import "./globals.css";
-import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
