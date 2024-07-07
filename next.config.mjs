e3adce65c847b7c007f4c2fc21904b70bf414459
YapYanTing <yanting@boostorder.com>
2024-07-08 03:17:50 +0800

    Add image file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-ALllow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-ALllow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-ALllow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                    {
+                        key: "Content-Range",
+                        value: "bytes : 0-9/*",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
